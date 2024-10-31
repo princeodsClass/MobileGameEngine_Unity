447d9f3c7e8f2def815173f867c18e7f2fc31a21
Soony <princeods@gmail.com>
2024-10-31 17:57:50 +0900

    - 수정 + 주석 추가.

--- 3DCore/Assets/Scripts/Statics/GameResourceManager.cs
+++ 3DCore/Assets/Scripts/Statics/GameResourceManager.cs
@@ -7,61 +7,79 @@ using UnityEngine.Networking;
 
 public partial class GameResourceManager : SingletonMono<GameResourceManager>
 {
+	// 전역 접근성.
+	// 관리하는 리소스의 일관성 ( 중복 처리 오류 ).
+	// 메모리 / 성능 관리 ( 최적화 ).
+	// 이 것들을 위해 singleton 처리.
+	
 	private Transform _tRootObjectPool = null;
+	private Dictionary<string, Queue<GameObject>> _dicObjectPool = new Dictionary<string, Queue<GameObject>>();
 
-	private Dictionary<string, Queue<GameObject>> m_dicObjectPool = new Dictionary<string, Queue<GameObject>>();
+	// Object Pooling 관련은 다음 시간에..!
 
-	private List<Dictionary<string, GameObject>> m_listCachedPrefabs = new List<Dictionary<string, GameObject>>();
-	private Dictionary<string, GameObject> m_dicUIPrefabs = new Dictionary<string, GameObject>();
-	private Dictionary<int, SpriteAtlas> m_dicAtlas = new Dictionary<int, SpriteAtlas>();
-	private Dictionary<string, Sprite> m_dicCachedSprite = new Dictionary<string, Sprite>();
-	private Dictionary<string, Object> m_dicCachedObject = new Dictionary<string, Object>();
+	private List<Dictionary<string, GameObject>> _listCachedPrefabs = new List<Dictionary<string, GameObject>>();
+	
+	// 프리팹은 중복 존재할 수 있으므로.
+	
+	private Dictionary<string, GameObject> _dicUIPrefabs = new Dictionary<string, GameObject>();
+	private Dictionary<int, SpriteAtlas> _dicAtlas = new Dictionary<int, SpriteAtlas>();
+	private Dictionary<string, Sprite> _dicCachedSprite = new Dictionary<string, Sprite>();
+	private Dictionary<string, Object> _dicCachedObject = new Dictionary<string, Object>();
 
-	public bool IsInitTableData { get; private set; }
+	// 중복되지 말아야 할 것들은 딕셔너리로 처리.
 
 	private void Awake()
 	{
-		transform.hasChanged = false;
-
-		if (0 == m_listCachedPrefabs.Count)
+		if (0 == _listCachedPrefabs.Count)
 		{
 			for (int i = 0; i < (int)EResourceType.End; ++i)
-				m_listCachedPrefabs.Add(new Dictionary<string, GameObject>());
+				_listCachedPrefabs.Add(new Dictionary<string, GameObject>());
 		}
 	}
 
 	private void OnDestroy()
 	{
-		m_dicAtlas.Clear();
-		m_dicCachedSprite.Clear();
-		m_dicCachedObject.Clear();
-		m_dicObjectPool.Clear();
+		// 소멸자 역할
 
-		for (int i = 0; i < m_listCachedPrefabs.Count; ++i)
-			m_listCachedPrefabs[i].Clear();
+		_dicAtlas.Clear();
+		_dicCachedSprite.Clear();
+		_dicCachedObject.Clear();
+		_dicObjectPool.Clear();
+
+		for (int i = 0; i < _listCachedPrefabs.Count; ++i)
+			_listCachedPrefabs[i].Clear();
 	}
 
 	public void Clear()
 	{
-		Dictionary<string, Queue<GameObject>>.Enumerator it = m_dicObjectPool.GetEnumerator();
+		Dictionary<string, Queue<GameObject>>.Enumerator it = _dicObjectPool.GetEnumerator();
 		while (it.MoveNext())
 			it.Current.Value.Clear();
 
-		m_dicObjectPool.Clear();
+		_dicObjectPool.Clear();
 		if (null != _tRootObjectPool)
 			_tRootObjectPool.DestroyChildren(false);
 
-		m_dicAtlas.Clear();
-		m_dicCachedSprite.Clear();
-		m_dicCachedObject.Clear();
+		_dicAtlas.Clear();
+		_dicCachedSprite.Clear();
+		_dicCachedObject.Clear();
 
-		for (int i = 0; i < m_listCachedPrefabs.Count; ++i)
-			m_listCachedPrefabs[i].Clear();
+		for (int i = 0; i < _listCachedPrefabs.Count; ++i)
+			_listCachedPrefabs[i].Clear();
 	}
 
+    /// <summary>
+	/// UI 프리팹들의 경로 반환 메서드
+	/// </summary>
+	/// <param name="eType"></param>
+	/// <param name="strPrefName"></param>
+	/// <param name="a_nTheme"></param>
+	/// <returns>
+	/// Path(EResourceType eType, string strPrefName, int a_nTheme = 0)
+	/// </returns>
 	public string GetPrefabPath(EResourceType eType, string strPrefName, int a_nTheme = 0)
-	{
-		string oThemePath = string.Format("{0:00}-Theme/", a_nTheme + 1);
+    {
+        string oThemePath = string.Format("{0:00}-Theme/", a_nTheme + 1);
 
 		switch (eType)
 		{
@@ -76,10 +94,20 @@ public partial class GameResourceManager : SingletonMono<GameResourceManager>
 		return string.Empty;
 	}
 
+	/// <summary>
+	/// 프리팹 로드 메서드
+	/// </summary>
+	/// <param name="eType"></param>
+	/// <param name="strPrefName"></param>
+	/// <param name="a_oOrigin"></param>
+	/// <param name="a_nTheme"></param>
+	/// <returns>
+	/// GameObject ( Prefab )
+	/// </returns>
 	public GameObject LoadPrefabs(EResourceType eType, string strPrefName, GameObject a_oOrigin = null, int a_nTheme = 0)
 	{
 		GameObject goPref;
-		if (!m_listCachedPrefabs[(int)eType].TryGetValue(strPrefName, out goPref))
+		if (!_listCachedPrefabs[(int)eType].TryGetValue(strPrefName, out goPref))
 		{
 			string strPath = this.GetPrefabPath(eType, strPrefName, a_nTheme);
 			goPref = Load<GameObject>(strPath) ?? a_oOrigin;
@@ -99,30 +127,33 @@ public partial class GameResourceManager : SingletonMono<GameResourceManager>
 				return null;
 			}
 
-			m_listCachedPrefabs[(int)eType].Add(strPrefName, goPref);
+			_listCachedPrefabs[(int)eType].Add(strPrefName, goPref);
 		}
 		return goPref;
 	}
 
+	/// <summary>
+	/// 리소스 로드 후 로드한 개체를 반환하는 제너릭 메서드
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="path"></param>
+	/// <returns>T</returns>
 	public T Load<T>(string path) where T : UnityEngine.Object
 	{
 		return Resources.Load<T>(path);
 	}
 
-	public IEnumerator LoadAsync<T>(string path, System.Action<T> callBack) where T : UnityEngine.Object
-	{
-		ResourceRequest req = Resources.LoadAsync<T>(path);
-		yield return req;
-		if (req.asset) callBack(req.asset as T);
-		if (req.asset) yield break;
-	}
-
-
+	/// <summary>
+	/// UI 프리팹 로드 후 게임 오브젝트를 반환하는 메서드
+	/// </summary>
+	/// <param name="eType"></param>
+	/// <param name="strPrefName"></param>
+	/// <returns>Prefab as GameObject</returns>
 	private GameObject LoadUIPrefabs(EResourceType eType, string strPrefName)
 	{
 		GameObject goPref;
 
-		if (!m_dicUIPrefabs.TryGetValue(strPrefName, out goPref))
+		if (!_dicUIPrefabs.TryGetValue(strPrefName, out goPref))
 		{
 			string strPath = GetPrefabPath(eType, strPrefName);
 			goPref = Load<GameObject>(strPath);
@@ -133,12 +164,24 @@ public partial class GameResourceManager : SingletonMono<GameResourceManager>
 				return null;
 			}
 
-			m_dicUIPrefabs.Add(strPrefName, goPref);
+			_dicUIPrefabs.Add(strPrefName, goPref);
 		}
 
 		return goPref;
 	}
-
+	
+	/// <summary>
+	/// 오브젝트 생성 후 생성 오브젝트 반환. 캐싱 과정 포함.
+	/// </summary>
+	/// <param name="eType"></param>
+	/// <param name="strPrefName"></param>
+	/// <param name="vPos"></param>
+	/// <param name="qRot"></param>
+	/// <param name="fLifeTime"></param>
+	/// <param name="bCached"></param>
+	/// <param name="a_oOrigin"></param>
+	/// <param name="a_nTheme"></param>
+	/// <returns>GameObject</returns>
 	public GameObject CreateObject(EResourceType eType, string strPrefName, Vector3 vPos, Quaternion qRot, float fLifeTime = 0f, bool bCached = true, GameObject a_oOrigin = null, int a_nTheme = 0)
 	{
 		if (null == _tRootObjectPool)
@@ -150,18 +193,18 @@ public partial class GameResourceManager : SingletonMono<GameResourceManager>
 
 		GameObject go = null;
 		string strObjName = strPrefName;
-		if (m_dicObjectPool.ContainsKey(strObjName))
+		if (_dicObjectPool.ContainsKey(strObjName))
 		{
-			if (0 != m_dicObjectPool[strObjName].Count)
+			if (0 != _dicObjectPool[strObjName].Count)
 			{
-				go = m_dicObjectPool[strObjName].Dequeue();
+				go = _dicObjectPool[strObjName].Dequeue();
 				if (null != go) go.transform.SetParent(null);
 			}
 		}
 		else
 		{
 			if (bCached)
-				m_dicObjectPool.Add(strObjName, new Queue<GameObject>());
+				_dicObjectPool.Add(strObjName, new Queue<GameObject>());
 		}
 
 		if (null == go)
@@ -188,6 +231,16 @@ public partial class GameResourceManager : SingletonMono<GameResourceManager>
 		return go;
 	}
 
+    /// <summary>
+	/// 오브젝트 생성 후 생성 오브젝트 반환
+	/// </summary>
+	/// <param name="eType"></param>
+	/// <param name="strPrefName"></param>
+	/// <param name="tParent"></param>
+	/// <param name="fLifeTime"></param>
+	/// <param name="bCached"></param>
+	/// <param name="a_nTheme"></param>
+	/// <returns></returns>
 	public GameObject CreateObject(EResourceType eType, string strPrefName, Transform tParent, float fLifeTime = 0f, bool bCached = true, int a_nTheme = 0)
 	{
 		GameObject go = CreateObject(eType, strPrefName, Vector3.zero, Quaternion.identity, fLifeTime, bCached, a_nTheme: a_nTheme);
@@ -197,19 +250,41 @@ public partial class GameResourceManager : SingletonMono<GameResourceManager>
 		return go;
 	}
 
-	public GameObject CreateObject(EResourceType eType, string strPrefName, Vector3 vec, bool bCached = true)
+    /// <summary>
+    /// 오브젝트 생성 후 생성 오브젝트 반환
+    /// </summary>
+    /// <param name="eType"></param>
+    /// <param name="strPrefName"></param>
+    /// <param name="vec"></param>
+    /// <param name="bCached"></param>
+    /// <returns></returns>
+    public GameObject CreateObject(EResourceType eType, string strPrefName, Vector3 vec, bool bCached = true)
 	{
 		GameObject go = CreateObject(eType, strPrefName, vec, Quaternion.identity, 0f, bCached);
 		return go;
 	}
 
-	public GameObject CreateObject(EResourceType eType, string strPrefName, bool bCached = true)
+    /// <summary>
+    /// 오브젝트 생성 후 생성 오브젝트 반환
+    /// </summary>
+    /// <param name="eType"></param>
+    /// <param name="strPrefName"></param>
+    /// <param name="bCached"></param>
+    /// <returns></returns>
+    public GameObject CreateObject(EResourceType eType, string strPrefName, bool bCached = true)
 	{
 		GameObject go = CreateObject(eType, strPrefName, Vector3.zero, Quaternion.identity, 0f, bCached);
 		return go;
 	}
 
-	public GameObject CreateUIObject(EResourceType eType, string strPrefName, Transform tParent)
+    /// <summary>
+    /// 오브젝트 생성 후 생성 오브젝트 반환
+    /// </summary>
+    /// <param name="eType"></param>
+    /// <param name="strPrefName"></param>
+    /// <param name="tParent"></param>
+    /// <returns></returns>
+    public GameObject CreateUIObject(EResourceType eType, string strPrefName, Transform tParent)
 	{
 		GameObject go = CreateObject(eType, strPrefName, Vector3.zero, Quaternion.identity);
 		if (null == go) return null;
@@ -219,7 +294,12 @@ public partial class GameResourceManager : SingletonMono<GameResourceManager>
 		return go;
 	}
 
-	private void CheckDestroyObject(GameObject go, float fLifeTime)
+    /// <summary>
+    /// 오브젝트 생성 후 생성 오브젝트 반환
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="fLifeTime"></param>
+    private void CheckDestroyObject(GameObject go, float fLifeTime)
 	{
 		AutoDestroy goAD = go.GetComponent<AutoDestroy>();
 		if (null == goAD) goAD = go.AddComponent<AutoDestroy>();
@@ -227,6 +307,11 @@ public partial class GameResourceManager : SingletonMono<GameResourceManager>
 		goAD.Init(fLifeTime);
 	}
 
+	/// <summary>
+	/// 생성된-로드된 오브젝트 삭제 메서드.
+	/// </summary>
+	/// <param name="go"></param>
+	/// <param name="bInsertPool"></param>
 	public void ReleaseObject(GameObject go, bool bInsertPool = true)
 	{
 		if (null == go) return;
@@ -236,14 +321,14 @@ public partial class GameResourceManager : SingletonMono<GameResourceManager>
 		{
 			if (bInsertPool)
 			{
-				if (!m_dicObjectPool.ContainsKey(go.name))
-					m_dicObjectPool.Add(go.name, new Queue<GameObject>());
+				if (!_dicObjectPool.ContainsKey(go.name))
+					_dicObjectPool.Add(go.name, new Queue<GameObject>());
 
 				go.SetActive(false);
 
-				if (!m_dicObjectPool[go.name].Contains(go))
+				if (!_dicObjectPool[go.name].Contains(go))
 				{
-					m_dicObjectPool[go.name].Enqueue(go);
+					_dicObjectPool[go.name].Enqueue(go);
 
 #if DISABLE_THIS
 					go.transform.SetParent(_tRootObjectPool);
@@ -262,163 +347,81 @@ public partial class GameResourceManager : SingletonMono<GameResourceManager>
 		}
 	}
 
+	/// <summary>
+	/// 스프라이트 아틀라스를 로드하고 반환하는 메서드.
+	/// </summary>
+	/// <param name="eAtlas"></param>
+	/// <returns></returns>
 	private SpriteAtlas GetAtlas(EAtlasType eAtlas)
 	{
 		int nKey = (int)eAtlas;
-		if (m_dicAtlas.ContainsKey(nKey))
-			return m_dicAtlas[nKey];
+		if (_dicAtlas.ContainsKey(nKey))
+			return _dicAtlas[nKey];
 
 		string strPath = $"{ComType.ATLAS_PATH}{eAtlas}";
 		SpriteAtlas atlas = Load<SpriteAtlas>(strPath);
 		if (null != atlas)
-			m_dicAtlas.Add(nKey, atlas);
+			_dicAtlas.Add(nKey, atlas);
 
 		return atlas;
 	}
 
+	/// <summary>
+	/// 로드된 스프라이트 아틀라스에서 스프라이트를 꺼내 반환하는 메서드
+	/// </summary>
+	/// <param name="eAtlas"></param>
+	/// <param name="strSpriteName"></param>
+	/// <returns></returns>
 	public Sprite LoadSprite(EAtlasType eAtlas, string strSpriteName)
 	{
-		if (m_dicCachedSprite.ContainsKey(strSpriteName))
-			return m_dicCachedSprite[strSpriteName];
+		if (_dicCachedSprite.ContainsKey(strSpriteName))
+			return _dicCachedSprite[strSpriteName];
 
 		SpriteAtlas atlas = GetAtlas(eAtlas);
 		if (null != atlas)
 		{
 			Sprite spr = atlas.GetSprite(strSpriteName);
-			if (null != spr) m_dicCachedSprite.Add(strSpriteName, spr);
+			if (null != spr) _dicCachedSprite.Add(strSpriteName, spr);
 			return spr;
 		}
 		return null;
 	}
 
+	/// <summary>
+	/// 텍스쳐 로드 후 반환하는 메서드.
+	/// </summary>
+	/// <param name="strTextureName"></param>
+	/// <returns></returns>
 	public Texture LoadTexture(string strTextureName)
 	{
-		if (m_dicCachedObject.ContainsKey(strTextureName))
-			return m_dicCachedObject[strTextureName] as Texture;
+		if (_dicCachedObject.ContainsKey(strTextureName))
+			return _dicCachedObject[strTextureName] as Texture;
 
 		Object obj = Load<Object>($"{ComType.TEXTURE_PATH}{strTextureName}");
 		if (null != obj)
 		{
-			m_dicCachedObject.Add(strTextureName, obj);
+			_dicCachedObject.Add(strTextureName, obj);
 			return obj as Texture;
 		}
 		return null;
 	}
 
+	/// <summary>
+	/// 머터리얼 로드 후 반환하는 메서드.
+	/// </summary>
+	/// <param name="strMaterialName"></param>
+	/// <returns></returns>
 	public Material LoadMaterial(string strMaterialName)
 	{
-		if (m_dicCachedObject.ContainsKey(strMaterialName))
-			return m_dicCachedObject[strMaterialName] as Material;
+		if (_dicCachedObject.ContainsKey(strMaterialName))
+			return _dicCachedObject[strMaterialName] as Material;
 
 		Object obj = Load<Object>($"{ComType.MATERIAL_PATH}{strMaterialName}");
 		if (null != obj)
 		{
-			m_dicCachedObject.Add(strMaterialName, obj);
+			_dicCachedObject.Add(strMaterialName, obj);
 			return obj as Material;
 		}
 		return null;
-	}
-
-	public void GetDownloadTexture(string strURL, string strFolderName, System.Action<Texture2D> callBack)
-	{
-		if (string.IsNullOrEmpty(strURL))
-		{
-			callBack.Invoke(null);
-			return;
-		}
-
-		if (m_dicCachedObject.ContainsKey(strURL))
-		{
-			GameManager.Log("yellow", $"============== Caching URL : {strURL}");
-			callBack.Invoke(m_dicCachedObject[strURL] as Texture2D);
-			return;
-		}
-
-		if (!File.Exists(ComUtil.GetFullFilePath(strURL, strFolderName)))
-		{
-			StartCoroutine(CoDownloadImage(strURL, strFolderName, callBack));
-			return;
-		}
-
-		byte[] bytes = File.ReadAllBytes(ComUtil.GetFullFilePath(strURL, strFolderName));
-		if (0 == bytes.Length)
-		{
-			callBack.Invoke(null);
-			return;
-		}
-
-		//GameManager.Log("yellow", $"============== LoadImage URL : {strURL}");
-		Texture2D tex = new Texture2D(128, 128, TextureFormat.ASTC_6x6, false);
-		tex.LoadImage(bytes);
-		callBack.Invoke(tex);
-	}
-
-	IEnumerator CoDownloadImage(string strURL, string strFolderName, System.Action<Texture2D> callBack)
-	{
-		UnityWebRequest cReq = UnityWebRequestTexture.GetTexture(strURL);
-		yield return cReq.SendWebRequest();
-
-		string strPath = ComUtil.GetFullFilePath(strURL, strFolderName);
-		string strDir = strPath.Substring(0, strPath.LastIndexOf('/') + 1);
-		if (!Directory.Exists(strDir)) Directory.CreateDirectory(strDir);
-
-		if (string.IsNullOrEmpty(cReq.error))
-		{
-			//GameManager.Log("yellow", $"============== CoDownloadImage SaveDir:{strDir}\n SavePath: {strPath}");
-
-			File.WriteAllBytes(strPath, cReq.downloadHandler.data);
-			Texture2D tex = ((DownloadHandlerTexture)cReq.downloadHandler).texture;
-			m_dicCachedObject.Add(strURL, tex);
-
-			callBack.Invoke(tex);
-		}
-		else
-		{
-			GameManager.Log($"Event CoDownloadImage Error: {cReq.error}", "red");
-			callBack.Invoke(null);
-		}
-	}
-
-	public IEnumerator GetRequest<T>(string strURL, System.Action<string> callBack)
-	{
-		UnityWebRequest cReq = UnityWebRequest.Get(strURL);
-		yield return cReq.SendWebRequest();
-
-		if (string.IsNullOrEmpty(cReq.error))
-		{
-			if (null != callBack)
-				callBack.Invoke(cReq.downloadHandler.text);
-		}
-		else
-		{
-			GameManager.Log($"GetRequestDownload Error: {cReq.error} URL:{strURL}", "red");
-		}
-	}
-
-	public GameObject CreateObject(GameObject a_oObj, Transform tParent, Transform a_oDummy, float fLifeTime = 0f, bool bCached = true, int a_nTheme = 0)
-	{
-		GameObject go = CreateObject(EResourceType.Custom, a_oObj.name, Vector3.zero, Quaternion.identity, fLifeTime, bCached, a_oObj, a_nTheme);
-		if (null == go) return null;
-
-		ComUtil.SetParent(tParent, go.transform);
-
-		// 더미가 존재 할 경우
-		if (a_oDummy != null)
-		{
-			go.transform.rotation = a_oDummy.rotation;
-			go.transform.position = a_oDummy.position;
-		}
-
-		return go;
-	}
-
-	public T CreateObject<T>(EResourceType eType, string strPrefName, Transform tParent, float fLifeTime = 0f, bool bCached = true, int a_nTheme = 0) where T : Component
-	{
-		return this.CreateObject(eType, strPrefName, tParent, fLifeTime, bCached, a_nTheme)?.GetComponentInChildren<T>();
-	}
-
-	public T CreateObject<T>(GameObject a_oObj, Transform tParent, Transform a_oDummy, float fLifeTime = 0f, bool bCached = true, int a_nTheme = 0) where T : Component
-	{
-		return this.CreateObject(a_oObj, tParent, a_oDummy, fLifeTime, bCached, a_nTheme)?.GetComponentInChildren<T>();
 	}
 }
